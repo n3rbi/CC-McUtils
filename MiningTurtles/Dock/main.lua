5f69c1192e1321ab9cbcfea0a72e7c4ff9872dd4
n3rbi <161168247+n3rbi@users.noreply.github.com>
2026-03-03 22:18:23 +0000

    Add files via upload

--- MiningTurtles/Dock/main.lua
+++ MiningTurtles/Dock/main.lua
@@ -1,4 +1,27 @@
 print("This is a Dock")
 
-local dock_pos = gps.locate()
-print("Dock position: " .. dock_pos)
+local dock_posX, dock_posY, dock_posZ = gps.locate()
+local modem = peripheral.find("modem")
+
+local facing
+
+function configure_facing()
+    -- find the modem, the modem is always behind the block. Use cords to determine facing North, East, South, West.
+
+    if modem then
+        local modem_posX, modem_posY, modem_posZ = gps.locate()
+        if modem_posX > dock_posX then
+            facing = "East"
+        elseif modem_posX < dock_posX then
+            facing = "West"
+        elseif modem_posZ > dock_posZ then
+            facing = "South"
+        elseif modem_posZ < dock_posZ then
+            facing = "North"
+        else
+            print("Error: Modem is at the same position as the dock.")
+        end
+    else
+        print("Error: No modem found. Please place a modem behind the dock.")
+    end
+end
